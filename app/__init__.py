bb4dfa9e8c53377bb46f1494710b73e54205315d
JoaquinMulet <138717300+JoaquinMulet@users.noreply.github.com>
2024-04-11 19:11:05 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hyqsowhe:rSB75Si9GrLRGZVU_OHQYk4xY3lXG4Rw@isabelle.db.elephantsql.com/hyqsowhe'
     app.config['SECRET_KEY'] = 'tu_clave_secreta_unicaysecreta'
     
     # Inicializa la extensión de la base de datos con la aplicación
